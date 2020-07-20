e5f5d709d6d990c730395215862f3a8dfaeed7a8
shinohara-san <someiyoshino0809@gmail.com>
2020-07-20 16:26:21 +0900

    Initial Commit

---
+++ MVCver_urlsession/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCver_urlsession
+//
+//  Created by Yuki Shinohara on 2020/07/20.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
